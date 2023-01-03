e90da45b0ebaf980ab8567c5993a5b465c180400
Chase Klapperich <chaseklapperich@arizona.edu>
2023-01-02 19:07:05 -0700

    AbstractBet created

---
+++ Craps/src/AbstractBet.java
@@ -0,0 +1,76 @@
+/*a
+ * Abstract class for all bets that can be made in a game of craps.
+ * Odds will need to be set by the subclasses depending on the bet made,
+ * and the betAmount will be set by the Method class that is using
+ * said AbstractBet.
+ * 
+ * @author Chase Klapperich
+ *
+ */
+abstract class AbstractBet {
+	private int[] odds = new int[2];
+	private int betAmount;
+	
+	
+	/**
+	 * Determines which numbers result in a win for the current bet 
+	 * 
+	 * @param roll The sum of the last dice roll
+	 * @return A boolean, whether or not the roll has resulted in a
+	 * 		   winning bet
+	 */
+	abstract boolean betWon(int roll);
+	
+	/**
+	 * Determines which numbers result in a loss for the current bet 
+	 * 
+	 * @param roll The sum of the last dice roll
+	 * @return A boolean, whether or not the roll has resulted in a
+	 * 		   winning bet
+	 */
+	abstract boolean betLost(int roll);
+	
+	/**
+	 * Calculates the winnings/loss after a given roll
+	 * 
+	 * @param roll - The sum of the last dice roll
+	 * @return An int, the winnings/loss of the roll
+	 */
+	public int processRoll(int roll) {
+		if (betWon(roll))
+			return betAmount / odds[1] * odds[0];
+		else if (betLost(roll))
+			return -betAmount;
+		else
+			return 0;
+	}
+	
+	/**
+	 * Increases the amount bet
+	 * 
+	 * @param increase - an int, the amount to increase by
+	 */
+	public void increaseBet(int increase) {
+		betAmount += increase;
+	}
+	
+	/**
+	 * Decreases the amount bet, will 0 out if there trying to decrease by more
+	 * than bet
+	 * 
+	 * @param decrease - an int, the amount to decrease by
+	 */
+	public void decreaseBet(int decrease) {
+		if(decrease > betAmount)
+			clearBet();
+		else
+			betAmount -= decrease;
+	}
+	
+	/**
+	 * Clears the amount bet
+	 */
+	public void clearBet() {
+		betAmount = 0;
+	}
+}
